2e28dd515a391d4ec73dc3a01c66c24ef228610d
Yuvy05 <sharmayuvraj052005@gmail.com>
2026-07-05 19:42:46 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.87%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st ;
+        for(auto i : s){
+            if(i == '(' or i == '{' or i == '['){
+                st.push(i) ;
+            }
+            else if(st.size()){
+                if(i == ')' and st.top()== '('){
+                    st.pop();
+                }
+                else if(i == ']' and st.top() == '['){
+                    st.pop();
+                }
+                else if(i == '}' and st.top()== '{'){
+                    st.pop();
+                }
+                else return false ;
+            }
+            else return false; 
+        }
+        return st.size() ? false : true ;
+    }
+};
