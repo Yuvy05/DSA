855443d93cada37f8ae249bc0391415ae21520d9
Yuvy05 <sharmayuvraj052005@gmail.com>
2026-07-14 21:00:57 +0530

    Time: 11 ms (20.45%), Space: 20.1 MB (24.69%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<vector<int>>adj(n);
+        for(auto i : prerequisites){
+            adj[i[1]].push_back(i[0]) ;
+        }
+        vector<int>indegree(n) ;
+        for(int i = 0 ; i < n ; i++){
+            for(auto j : adj[i]){
+                indegree[j]++ ;
+            }
+        }
+        queue<int>q ;
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 0){
+                q.push(i) ;
+            }
+        }
+        vector<int>ans ;
+        while(q.size()){
+            int curr = q.front() ; q.pop() ;
+            ans.push_back(curr) ;
+            for(auto i : adj[curr]){
+                indegree[i]-- ;
+                if(indegree[i] == 0) q.push(i) ;
+            }
+            
+        }
+        if(ans.size() == n)return true ;
+        return false ;
+    }
+};
