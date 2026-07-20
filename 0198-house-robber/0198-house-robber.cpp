c8832176e58d377e28629010df3570da3cd1c46e
Yuvy05 <sharmayuvraj052005@gmail.com>
2026-07-20 12:05:17 +0530

    Time: 0 ms (100%), Space: 10.9 MB (6.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,6 +16,14 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size() ;
         vector<int>dp(nums.size(),-1) ;
-        return f(n-1 , nums,dp) ;
+        dp[0] = nums[0] ;
+        //dp[1] = cost[1] ;
+        for(int i = 1 ; i < n ; i++){
+            int take = nums[i] ; if (i>1) take +=  dp[i-2]  ;
+            int notake = 0 + dp[i-1] ;
+            dp[i] = max(take,notake) ;
+        }
+        return dp[n-1] ;
+        //return f(n-1 , nums,dp) ;
     }
 };
