81a736394675dc7c60fc81a6bb6616b596707b6d
Yuvy05 <sharmayuvraj052005@gmail.com>
2026-07-20 12:10:21 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,14 +16,31 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size() ;
         vector<int>dp(nums.size(),-1) ;
-        dp[0] = nums[0] ;
-        //dp[1] = cost[1] ;
-        for(int i = 1 ; i < n ; i++){
-            int take = nums[i] ; if (i>1) take +=  dp[i-2]  ;
-            int notake = 0 + dp[i-1] ;
-            dp[i] = max(take,notake) ;
-        }
-        return dp[n-1] ;
+
+        // tabulation
+
+        // dp[0] = nums[0] ;
+        // //dp[1] = cost[1] ;
+        // for(int i = 1 ; i < n ; i++){
+        //     int take = nums[i] ; if (i>1) take +=  dp[i-2]  ;
+        //     int notake = 0 + dp[i-1] ;
+        //     dp[i] = max(take,notake) ;
+        // }
+        // return dp[n-1] ;
+
+        // memoization
         //return f(n-1 , nums,dp) ;
+
+        //space 
+        int prev = nums[0] ;
+        int prev2 = 0 ;
+        for(int i = 1 ; i < n ; i++){
+            int take = nums[i] ; if (i>1) take += prev2  ;
+            int notake = 0 + prev ;
+            int curr = max(take,notake) ;
+            prev2 = prev ;
+            prev = curr ;
+        }
+        return prev ;
     }
 };
