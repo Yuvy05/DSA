3a732f27f1a96c08d424bfc39ebdd40cf76dd34e
Yuvy05 <sharmayuvraj052005@gmail.com>
2026-07-27 19:09:09 +0530

    Time: 27 ms (56.89%), Space: 27.6 MB (31.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -10,7 +10,25 @@ public:
         return dp[i][j] =  0 + max(rec(i-1,j,s1,s2,dp) , rec(i,j-1,s1,s2,dp)) ;
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>>dp(text1.size()+1 , vector<int>(text2.size()+1,-1)) ;
-        return rec(text1.size() , text2.size() , text1 , text2,dp) ;
+        vector<vector<int>>dp(text1.size()+1 , vector<int>(text2.size()+1,0)) ;
+        //return rec(text1.size() , text2.size() , text1 , text2,dp) ;
+        for(int i = 1 ; i <= text1.size() ; i++){
+            dp[i][0] = 0 ;
+        }
+        for(int i = 1 ; i <= text2.size() ; i++){
+            dp[0][i] = 0 ;
+        }
+        string s1 = text1 , s2 = text2 ;
+        for(int i = 1 ; i <= text1.size() ; i++){
+            for(int j = 1 ; j <= text2.size() ; j++){
+                // if(dp[i][j] != -1) return dp[i][j] ;
+                // match
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1] ;
+                // not match
+                else dp[i][j] =  0 + max(dp[i-1][j],dp[i][j-1]) ;
+            }
+        }
+        return dp[text1.size()][text2.size()] ;
     }
 };
