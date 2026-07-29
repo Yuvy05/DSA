cfb071a5a0150dbceaaf717fabd2cfd55c7a160c
Yuvy05 <sharmayuvraj052005@gmail.com>
2026-07-29 06:33:04 +0530

    Time: 39 ms (35.6%), Space: 37.1 MB (24.92%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rec(int idx , int amount , vector<int>&coins , vector<vector<int>>&dp){
+        //base case
+        if(idx == coins.size()) return 0 ;
+        if(amount == 0) return 1 ;
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int pick = 0 , notpick = 0 ;
+        //pick 
+        if(amount >= coins[idx]){
+            pick = rec(idx , amount - coins[idx] , coins,dp) ;
+        }
+        //not pick
+        notpick = rec(idx+1 , amount , coins,dp) ;
+        return dp[idx][amount] =  pick+notpick ;
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>>dp(coins.size()+1 , vector<int>(amount+1 , -1)) ;
+        return rec(0,amount , coins ,dp) ;
+    }
+};
